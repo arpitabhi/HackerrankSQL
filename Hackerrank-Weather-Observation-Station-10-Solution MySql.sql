cd36773be17311e0a2341506f045740f36635252
Arpit Shukla <arpitabhi@gmail.com>
2020-05-28 00:46:02 +0530

    Hackerrank Weather Observation Station 10 Solution MySql

---
+++ Hackerrank-Weather-Observation-Station-10-Solution MySql.sql
@@ -0,0 +1,9 @@
+#Author : Arpit Shukla
+#Date : 28th May, 2020
+
+#Hackerrank Weather Observation Station 10 Solution MySql
+
+select distinct city from station where city regexp "[^aieouAIEOU]$";
+
+
+
