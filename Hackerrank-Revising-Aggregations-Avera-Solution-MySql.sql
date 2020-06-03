ca21706bb3068f80bd7472d2ad36577f6594598c
Arpit Shukla <arpitabhi@gmail.com>
2020-06-03 22:08:39 +0530

    Hackerrank Revising Aggregations - Avera Solution MySql

---
+++ Hackerrank-Revising-Aggregations-Avera-Solution-MySql.sql
@@ -0,0 +1,7 @@
+/*
+# Author : Arpit Shukla
+# Date : 3rd June, 2020
+
+# Hackerrank Revising Aggregations - Avera Solution MySql
+*/
+select avg(population) from city where district = "California";
