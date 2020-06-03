5aa305485d2b7661ff401042a6fb82c6fbff6685
Arpit Shukla <arpitabhi@gmail.com>
2020-06-04 03:13:07 +0530

    Hackerrank Weather Observation Station 16 Solution MySql

---
+++ Hackerrank-Weather-Observation-Station-16-Solution MySql.sql
@@ -0,0 +1,10 @@
+/*
+Author : Arpit Shukla
+Date : 4th June, 2020
+
+Hackerrank Weather Observation Station 16 Solution MySql
+*/
+
+select round(min(lat_n),4) from station where lat_n >38.7780;
+
+
