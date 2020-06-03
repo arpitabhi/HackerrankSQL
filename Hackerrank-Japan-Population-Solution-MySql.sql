32f90d46c8992003250970bc9c8548faa0aa3bdb
Arpit Shukla <arpitabhi@gmail.com>
2020-06-03 22:19:51 +0530

    Hackerrank Population Density Difference Solution MySql

---
+++ Hackerrank-Japan-Population-Solution-MySql.sql
@@ -0,0 +1,9 @@
+/*
+# Author : Arpit Shukla
+# Date : 3rd June, 2020
+
+# Hackerrank Japan Population Solution MySql
+
+*/
+
+select sum(population) from city where countrycode="JPN";
---
+++ Hackerrank-Population-Density-Difference-Solution-MySql.sql
@@ -0,0 +1,9 @@
+/*
+Author : Arpit Shukla
+Date : 3rd June, 2020
+
+Hackerrank Population Density Difference Solution MySql
+
+*/
+
+select max(population) - min(population) from city;
